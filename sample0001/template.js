cec20897e460b9c163ee9cdb76c0fd10b186b60d
Masataka <takamasa.0412@icloud.com>
2019-12-20 16:33:20 +0900

    sample0001

---
+++ sample0001/template.js
@@ -0,0 +1,6 @@
+function funcAdd1() {
+  document.getElementById("sample").innerHTML = parseInt(document.getElementById("sample").firstChild.nodeValue) + 1;
+}
+function funcAdd2() {
+  document.getElementById("sample").innerHTML = parseInt(document.getElementById("sample").firstChild.nodeValue) + 2;
+}
