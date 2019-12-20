d2ef69239fd5a5d212ee65831aeeed2a1b9aef39
Masataka <takamasa.0412@icloud.com>
2019-12-20 20:30:36 +0900

    sample0004

---
+++ sample0004/template.js
@@ -0,0 +1,12 @@
+window.onload = function () {
+  document.getElementById("radio2").click();
+}
+function funcRed() {
+  document.getElementById("sample").style.backgroundColor = 'red';
+}
+function funcGreen() {
+  document.getElementById("sample").style.backgroundColor = 'green';
+}
+function funcBlue() {
+  document.getElementById("sample").style.backgroundColor = 'blue';
+}
