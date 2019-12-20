c0684bbe47a9dff3951f017b7188f9daec2a974e
Masataka <takamasa.0412@icloud.com>
2019-12-20 17:08:43 +0900

    sample0002

---
+++ sample0002/template.js
@@ -0,0 +1,9 @@
+function funcRed() {
+  document.getElementById("sample").style.backgroundColor = 'red';
+}
+function funcGreen() {
+  document.getElementById("sample").style.backgroundColor = 'green';
+}
+function funcBlue() {
+  document.getElementById("sample").style.backgroundColor = 'blue';
+}
