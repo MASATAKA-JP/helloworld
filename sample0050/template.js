8a3d414ff770cfd13c05db468aa89f053f2b2ffc
Masataka <takamasa.0412@icloud.com>
2019-12-29 21:53:54 +0900

    配列と配列を連結

---
+++ sample0050/template.js
@@ -0,0 +1,6 @@
+var $sampleArrayA = new Array('A1', 'A2', 'A3');
+var $sampleArrayB = new Array('B1', 'B2', 'B3');
+var $sampleArrayConcat = $sampleArrayA.concat($sampleArrayB);
+document.write('配列「' + $sampleArrayA + '」と、<br/>');
+document.write('配列「' + $sampleArrayB + '」を連結すると、<br/>');
+document.write('「' + $sampleArrayConcat + '」<br/>');
